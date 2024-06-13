1f7ecdc70a9089bee6687a238ae211d2a4b7c3c0
kilelx <llx.kieran@gmail.com>
2024-06-13 17:42:36 +0200

    fix menu issue

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const burgerMenu = document.querySelector('#nav__burger');
 const navMobile = document.querySelector('.nav__mobile');
-// const header = document.querySelector('header');
 
 burgerMenu.addEventListener('click', () => {
     navMobile.classList.toggle('show');
+    if(navMobile.classList.contains('show')) {
+        document.body.style.overflowY = 'hidden';
+        } else {
+        document.body.style.overflowY = 'visible';
+    }
 })
