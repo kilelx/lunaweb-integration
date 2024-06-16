7e8e6620c17139e68f93ef75af99e4314496b887
kilelx <llx.kieran@gmail.com>
2024-06-16 17:30:20 +0200

    fix: issue on mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const burgerMenu = document.querySelector('#nav__burger');
+const burgerMenu = document.querySelector('.nav__burger');
 const navMobile = document.querySelector('.nav__mobile');
 
 burgerMenu.addEventListener('click', () => {
     navMobile.classList.toggle('show');
     if(navMobile.classList.contains('show')) {
         document.body.style.overflowY = 'hidden';
-        } else {
+        document.body.style.maxHeight = '100vh';
+    } else {
         document.body.style.overflowY = 'visible';
+        document.body.style.maxHeight = 'auto';
     }
 })
