85754ea3e7fedb3403399459b7e9a03dbf4717bf
kilelx <llx.kieran@gmail.com>
2024-06-16 18:46:43 +0200

    add animation on burger menu

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const navMobile = document.querySelector('.nav__mobile');
 
 burgerMenu.addEventListener('click', () => {
     navMobile.classList.toggle('show');
+    burgerMenu.classList.toggle('show');
     if(navMobile.classList.contains('show')) {
-        document.body.style.overflowY = 'hidden';
-        // document.body.style.maxHeight = '100vh';
-    } else {
-        document.body.style.overflowY = 'visible';
-        // document.body.style.maxHeight = 'auto';
+        document.body.style.overflow = 'hidden';
+        } else {
+        document.body.style.overflow = 'visible';
     }
 })
