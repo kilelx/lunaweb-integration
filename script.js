1d7e68dd1e6b297df964ee22c976b28af7607d3c
kilelx <llx.kieran@gmail.com>
2024-06-17 08:48:25 +0200

    add README

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const burgerMenu = document.querySelector('.nav__burger');
 const navMobile = document.querySelector('.nav__mobile');
 
 burgerMenu.addEventListener('click', () => {
-    navMobile.classList.toggle('show');
+
+    // Animer le menu burger
     burgerMenu.classList.toggle('show');
+
+    // Ouvrir le menu burger
+    navMobile.classList.toggle('show');
     if(navMobile.classList.contains('show')) {
         document.body.style.overflow = 'hidden';
         document.body.style.maxHeight = '100vh';
